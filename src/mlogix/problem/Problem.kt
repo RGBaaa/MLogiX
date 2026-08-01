ad38ba24d7c41ba82360c2b4570946fabdfe5edb
Kaleinova <Kaleinova@outlook.com>
2026-08-01 22:13:10 +0800

    refactor(Problem): streamline line info retrieval and enhance output formatting

--- src/mlogix/problem/Problem.kt
+++ src/mlogix/problem/Problem.kt
@@ -13,16 +13,7 @@ abstract class Problem(
     val level: ProblemLevel     // 问题级别（错误或警告）
 ) {
     val lineInfos = Seq<LineInfo>()
-
-    // 获取行，若不存在则新建并返回
-    private fun getLineInfo(line: Int): LineInfo {
-        for (lineInfo in lineInfos) {
-            if (lineInfo.line == line) return lineInfo
-        }
-        val lineInfo = LineInfo(line, sourceMap.getLineString(line))
-        lineInfos.add(lineInfo)
-        return lineInfo
-    }
+    var centerLine = null as LineInfo?
 
     fun point(obj: Spanned, text: String): Problem {
         val span = obj.span()
@@ -30,9 +21,13 @@ abstract class Problem(
     }
 
     fun point(start: Int, end: Int, text: String): Problem {
-        val lineInfo = getLineInfo(sourceMap.getLine(start))
+        val line = sourceMap.getLine(start)
+        val lineInfo = getLineInfo(line)
+        if (centerLine == null) centerLine = lineInfo
+        val col = sourceMap.getCol(start)
+
         lineInfo.point(
-            sourceMap.getCol(start),
+            col,
             "^".repeat(max(1, end - start)),
             text
         )
@@ -54,15 +49,33 @@ abstract class Problem(
         return this
     }
 
+    // 获取行，若不存在则新建并返回
+    private fun getLineInfo(line: Int): LineInfo {
+        for (lineInfo in lineInfos) {
+            if (lineInfo.line == line) return lineInfo
+        }
+        val lineInfo = LineInfo(line, sourceMap.getLineString(line))
+        lineInfos.add(lineInfo)
+        return lineInfo
+    }
+
     override fun toString(): String {
         val color = if (level == ProblemLevel.ERROR) Ansi.RED else Ansi.YELLOW
-        val str = StringBuilder("$color${level.name}:$problemName${Ansi.DEFAULT}\n")
+        val str = StringBuilder("$color${level.name.lowercase()}: $problemName${Ansi.DEFAULT}\n")
         lineInfos.sort(Comparator.comparing { li -> li.line })
 
         var maxLineDigitLen = 0  // 所有 LineInfo 中最长的行号长度
         for (lineInfo in lineInfos) {
             maxLineDigitLen = maxOf(maxLineDigitLen, lineInfo.line.toString().length)
         }
+
+        //  --> Path:line:col
+        str.append(" ".repeat(maxLineDigitLen - 1))
+            .append(" --> ")
+            .append(sourceMap.relativePath)
+            .append(":").append(centerLine?.line ?: lineInfos[0].line)
+            .append(":").append(centerLine?.col ?: lineInfos[0].col)
+            .append("\n")
 
         for (lineInfo in lineInfos) {
             str.append(lineInfo.format(maxLineDigitLen))
@@ -92,11 +105,11 @@ abstract class Problem(
     data class Info(val col: Int, val indicator: String, val text: String)
 
     // 储存一行的错误信息
-    inner class LineInfo(
+    class LineInfo(
         val line: Int,
         val lineString: String
     ) {
-        val infos = mutableListOf<Info>()
+        val infos = Seq<Info>()
         var col: Int = 0
 
         fun point(startCol: Int, indicator: String, text: String) {
@@ -114,70 +127,68 @@ abstract class Problem(
         fun format(maxLineDigitLen: Int): String {
             val str = StringBuilder()
 
-            // -->Path:line:col
-            str.append(" ".repeat(maxLineDigitLen - 1))
-                .append("-->")
-                .append(sourceMap.relativePath).append(":").append(line).append(":").append(col).append("\n")
+            //   ┃
+            str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append(" ┃ ").append(Ansi.DEFAULT).append("\n")
 
-            //  ┃
-            str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append("┃").append(Ansi.DEFAULT).append("\n")
-
-            // L┃lineString
+            // L ┃ lineString
             val lineDigitLen = line.toString().length
             str.append(Ansi.CYAN)
-                .append(" ".repeat(maxLineDigitLen - lineDigitLen)).append(line).append("┃")
+                .append(" ".repeat(maxLineDigitLen - lineDigitLen)).append(line).append(" ┃ ")
                 .append(Ansi.DEFAULT).append(lineString).append("\n")
 
-            if (infos.isEmpty()) return str.toString()  // 为空退出防止越界
+            if (infos.isEmpty) return str.toString()  // 为空退出防止越界
 
-            infos.sortBy { it.col }  // 排序，以从前往后输出
+            // 先保证后加入的靠下，后保证顺序
+            infos.sortComparing { info -> info.col }
 
-            //  ┃ ^ - ^ infos[last]text
-            str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append("┃").append(Ansi.DEFAULT)
-            var col = 1  // 标识当前输出列
-            for ((col1, indicator) in infos) {
-                val count = col1 - col
+            //   ┃  ^ - ^ infos[last]text
+            str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append(" ┃ ").append(Ansi.DEFAULT)
+            var curCol = 1  // 标识当前输出列
+            for ((col, indicator) in infos) {
+                val count = col - curCol
                 if (count < 0) continue  // 与上一个重叠
                 str.append(" ".repeat(count))
                 str.append(indicator)
-                col = col1 + indicator.length
+                curCol = col + indicator.length
             }
+            // 先保证后加入的靠下，后保证顺序
+            infos.reverse().sortComparing { info -> info.col }
             str.append(" ").append(infos.last().text)
             str.append("\n")
 
-            //  ┃ | |
-            //  ┃ | infos[last-1].text
-            //  ┃ |
-            //  ┃ infos[last-2].text
+            //   ┃  | |
+            //   ┃  | infos[last-1].text
+            //   ┃  |
+            //   ┃  infos[last-2].text
             for (i in infos.size - 1 downTo 1) {
                 val text = infos[i - 1].text
                 if (text.isEmpty()) continue
 
-                //  ┃ | |
-                str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append("┃").append(Ansi.DEFAULT)
-                col = 1
+                //   ┃  | |
+                str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append(" ┃ ").append(Ansi.DEFAULT)
+                curCol = 1
                 for (j in 0 until i) {
-                    val count = infos[j].col - col
+                    val count = infos[j].col - curCol
                     if (count < 0) continue  // 重叠
                     str.append(" ".repeat(count))
                     str.append("|")
-                    col = infos[j].col + 1
+                    curCol = infos[j].col + 1
                 }
                 str.append("\n")
 
                 //  ┃ | infos[last-1].text
-                str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append("┃").append(Ansi.DEFAULT)
-                col = 1
+                str.append(Ansi.CYAN).append(" ".repeat(maxLineDigitLen)).append(" ┃ ").append(Ansi.DEFAULT)
+                curCol = 1
                 for (j in 0 until i - 1) {
                     if (infos[j].col == infos[j + 1].col) {  // 与后一个重叠
                         continue  // 跳过此次
                     }
-                    val count = infos[j].col - col
+                    val count = infos[j].col - curCol
                     str.append(" ".repeat(count))
                     str.append("|")
-                    col = infos[j].col + 1
+                    curCol = infos[j].col + 1
                 }
-                str.append(" ".repeat(maxOf(0, infos[i - 1].col - col)))
+                str.append(" ".repeat(maxOf(0, infos[i - 1].col - curCol)))
                 str.append(text)
                 str.append("\n")
             }
